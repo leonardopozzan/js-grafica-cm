8cab3ed741a8af1b3ff62737fb3fbb3557d07ffa
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2022-10-17 16:03:32 +0200

    creato griglia con l'evento al click delle celle

--- js/script.js
+++ js/script.js
@@ -9,3 +9,47 @@
 // - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 // - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 
+{/* <div class="grid">
+    <div class="square-easy">
+        <span>1</span>
+    </div>
+</div> */}
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click',play);
+const container = document.querySelector('.the-game');
+
+function play(){
+    container.innerHTML = '';
+    const difficulty = document.querySelector('#difficulty').value;
+
+    let numCell;
+    switch (difficulty) {
+        case 'easy':
+            numCell = 100
+            break;
+        case 'hard':
+            numCell = 81
+            break;
+        case 'crazy':
+            numCell = 49
+            break;
+    }
+    function drawGrid(){
+        const grid = addElementClassHTML('div', 'grid', container);
+        for (let i = 1; i <= numCell; i++){
+            createCell(i,grid);
+        }
+    }
+    function createCell(numb, grid){
+        const cell = addElementClassHTML('div', 'square', grid);
+        cell.innerHTML = numb
+        cell.style.width = `calc(100% / ${Math.sqrt(numCell)})`
+        cell.style.height = `calc(100% / ${Math.sqrt(numCell)})`
+        cell.addEventListener('click', function(){
+            cell.classList.add('checked')
+        })
+    }
+    drawGrid()
+
+}
